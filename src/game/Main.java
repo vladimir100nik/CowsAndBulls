39698770a79cf859b40b7b2155ec42421e7cb5b6
Vladimir <vladimir100nik@gmail.com>
2018-10-23 19:11:02 +0300

    new Scene

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,6 +1,8 @@
 package game;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class  Main extends Application {
@@ -11,6 +13,11 @@ public class  Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 500,300);
+        primaryStage.setTitle("Bulls and Cows");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 }
