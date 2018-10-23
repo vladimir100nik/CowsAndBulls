69bbac9215c14083a7e6325308deabb870f85e5d
Vladimir <vladimir100nik@gmail.com>
2018-10-23 19:43:59 +0300

    new scene1

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,11 +1,17 @@
 package game;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class  Main extends Application {
+public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
@@ -13,8 +19,21 @@ public class  Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Pane root = new Pane();
-        Scene scene = new Scene(root, 500,300);
+        Pane root = new VBox();
+//        Button btn = new Button("Hello");
+        Label lbl =new Label("It's Java fx");
+        TextField txt = new TextField();
+        lbl.textProperty().bind(
+                Bindings.concat("Hello, ", txt.textProperty(), "!"));
+
+//        btn.setOnAction(e -> {
+//            lbl.setText("Hello " + txt.getText() + "!");
+
+//        });
+
+        root.getChildren().addAll(lbl, txt);
+
+        Scene scene = new Scene(root, 500, 300);
         primaryStage.setTitle("Bulls and Cows");
         primaryStage.setScene(scene);
         primaryStage.show();
